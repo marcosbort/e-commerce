9052449309b1c6dad496afeaf4361648680dd82e
marcosbort <marcosbort@gmail.com>
2022-10-10 12:58:19 -0300

    css global details

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <meta name="description" content="e-commerce with Next.js and Typescript" />
         <link rel="icon" type="favicon/x-con" href="https://marcosbort.github.io/favicon-mb/favicon.png" />
       </Head>
-      <ProductContainer />
+      {/* <ProductContainer /> */}
       <Product />
     </>
   )
