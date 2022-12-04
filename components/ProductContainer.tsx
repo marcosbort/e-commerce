1908538b91e7b198ebeb9772759050bd7742ca08
marcosbort <marcosbort@gmail.com>
2022-12-03 21:21:17 -0300

    added first general styles

--- components/ProductContainer.tsx
+++ components/ProductContainer.tsx
@@ -12,9 +12,9 @@ export default function ProductContainer() {
 
   const getProducts = useCallback(async () => {
     setIsLoading(true)
-    const { data } = await axios.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQh35kh4HEg8CJd044vWDVgGa3laneMWv-1BxiG2xI09MByo4LEAdGxPpraA5wTbZw9CvJcDTb806vZ/pub?gid=0&single=true&output=csv', {
-      responseType: 'blob'
-    })
+    const { data } = await axios.get(
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQh35kh4HEg8CJd044vWDVgGa3laneMWv-1BxiG2xI09MByo4LEAdGxPpraA5wTbZw9CvJcDTb806vZ/pub?gid=0&single=true&output=csv', { responseType: 'blob', }
+    )
     Papa.parse(data, {
       header: true,
       complete: (results) => setProducts(results.data as ProductType[]),
@@ -31,27 +31,26 @@ export default function ProductContainer() {
 
   return (
     <div className={styles['ProductContainer']}>
-      <img className={styles['ProductContainer__header']} src='https://marcosbort.github.io/server/images/pet-food/header-web-1.png' alt="foto" />
-    <div className={styles['ProductContainer__product-box']}>
-      {isLoading
-        ? (
+      <img className={styles['ProductContainer__header']} src="https://marcosbort.github.io/server/images/pet-food/header-web-1.png" alt="foto" />
+      <div className={styles['ProductContainer__product-box']}>
+        {isLoading ? (
           <h2 className={styles['ProductContainer__loading']}>Loading...</h2>
         ) : (
-          products?.map((product, index) =>
+          products?.map((product, index) => (
             <div key={product.id}>
-                <Product
-                  key={product.id}
-                  id={product.id}
-                  brand={product.brand}
-                  description={product.description}
-                  category={product.category}
-                  image={product.image}
-                  price={product.price}
-                />
+              <Product
+                key={product.id}
+                id={product.id}
+                brand={product.brand}
+                description={product.description}
+                category={product.category}
+                image={product.image}
+                price={product.price}
+              />
             </div>
-          )
+          ))
         )}
-    </div>
+      </div>
     </div>
   )
 }
